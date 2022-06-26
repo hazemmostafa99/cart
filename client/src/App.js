fa10327028899f0020924eba381a24cd093478b8
hazemmostafa99 <hazem77mostafa90@gmail.com>
2022-06-26 15:05:19 +0200

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+// import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <div className="layout">
+      {/* <Header /> */}
+      <header>Shopping Cart</header>
+      <main>Content</main>
+      <footer>Footer</footer>
+    </div>
+  );
+}
+
+export default App;
